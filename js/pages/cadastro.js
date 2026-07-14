b6235a4d837291a927115b28b11e29a0f67acad4
Wilgner <devwgdossantos@gmail.com>
2026-07-14 17:49:33 -0300

    feat: integração api ViaCep

---
+++ js/pages/cadastro.js
@@ -0,0 +1,18 @@
+import { buscarCep } from "../services/viacep.js";
+
+let from = document.querySelector('#form-cadastro')
+let campoCep = document.querySelector('#cep')
+
+campoCep.addEventListener('blur', async() => {
+    if(!campoCep.value) return
+
+    try {
+        let endereco = await buscarCep(campoCep.value)
+        document.querySelector('#logradouro').value = endereco.logradouro
+        document.querySelector('#bairro').value = endereco.bairro
+        document.querySelector('#cidade').value = endereco.localidade
+        document.querySelector('#uf').value = endereco.uf
+        } catch(error) {
+            console.error(error.message)
+        }
+})
