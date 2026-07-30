7a2761d3823a8cfa96cb9e60825cc4dadaae6ed8
Wilgner Guimarães <devwgdossantos@gmail.com>
2026-07-30 11:53:38 -0300

    fix: alterado header

--- js/components/site-header.js
+++ js/components/site-header.js
@@ -18,6 +18,11 @@ class SiteHeader extends HTMLElement {
 
             <nav class="navbar">
                 <a class="logo" href="#"><img src="assets/images/logo-pao-dos-pobres.png" alt="Logo Fundação O Pão dos Pobres"></a>
+                <div class="mobile-menu">
+                    <div class="line1"></div>
+                    <div class="line2"></div>
+                    <div class="line3"></div>
+                </div>
                 <ul class="nav-list">
                     <li><a href="#">Home</a></li>
                     <li><a href="#cursos">Cursos</a></li>
@@ -29,6 +34,49 @@ class SiteHeader extends HTMLElement {
         </header>
         `
             ;
+
+ class MobileNavbar {
+                constructor(mobileMenu, navList, navLinks) {
+                    this.mobileMenu = document.querySelector(mobileMenu);
+                    this.navList = document.querySelector(navList)
+                    this.navLinks = document.querySelectorAll(navLinks)
+                    this.activeClass = "active";
+
+                    this.handleClick = this.handleClick.bind(this);
+                }
+
+                animateLinks() {
+                    this.navLinks.forEach((link, index) => {
+                        link.style.animation
+                            ? (link.style.animation = "")
+                            : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+                    });
+                }
+
+                handleClick() {
+                    this.navList.classList.toggle(this.activeClass);
+                    this.mobileMenu.classList.toggle(this.activeClass);
+                    this.animateLinks();
+                }
+
+                addClickEvent() {
+                    this.mobileMenu.addEventListener("click", this.handleClick);
+                }
+
+                init() {
+                    if (this.mobileMenu) {
+                        this.addClickEvent();
+                    }
+                    return this;
+                }
+            }
+
+            const mobileNavbar = new MobileNavbar(
+                ".mobile-menu",
+                ".nav-list",
+                ".nav-list li",
+            );
+            mobileNavbar.init();
     }
 }
 
